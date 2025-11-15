5d65ac8a7b6faa84dcaa0ec085dd352444b297d3
brian <brianarguello.vga>
2025-11-14 23:26:34 -0300

    hola mundo en dart

---
+++ 01-dart/helloWorld.dart
@@ -0,0 +1,11 @@
+void main() {
+  
+  var myName = "Brian";
+  
+  late final myLastName;
+  
+  myLastName = "Arguello";
+  String nombreCompleto = "$myName $myLastName";
+  print("Hola ${nombreCompleto.toUpperCase()}");
+  
+}
