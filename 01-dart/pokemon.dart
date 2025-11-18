c9dc11aa09443c990e092b6dfde7cd79400a1d28
brian <brianarguello.vga>
2025-11-18 11:58:29 -0300

    dynamic

--- 01-dart/pokemon.dart
+++ 01-dart/pokemon.dart
@@ -6,12 +6,23 @@ void main() {
   final List<String> habilidades = ["ataque"];
   final sprites = <String> ["ataque.png", "defensa.png"];
   
+  // esto es un comentario
+  // dynamic
+  
+  
+  dynamic errormensaje = "hola";
+  errormensaje = true;
+  errormensaje = {1,2,3,4,5};
+  errormensaje = null;
+  
+  
   print("""
   $pokemon
   $hp
   $isAlive
   $habilidades
   $sprites
+  $errormensaje
   """);
   
 }
